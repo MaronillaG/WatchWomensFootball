1eacf5b1e95ac63080b4a828229617d3cf04d81f
Gillian <105639251+MaronillaG@users.noreply.github.com>
2023-05-02 16:11:19 +0200

    added date generation and displays on page

---
+++ script.js
@@ -0,0 +1,13 @@
+const date = new Date();
+
+let day = date.getDate();
+let month = date.getMonth() + 1;
+let year = date.getFullYear();
+
+let today = `${day}-${month}-${year}`;
+
+let showDate = document.querySelector('#date');
+
+showDate.innerText = today;
+
+console.log(showDate)
